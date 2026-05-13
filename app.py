be9826cd4ba76636ff32047d44488a018ada16ff
ozoglualtar2-cloud <ozoglualtar2@gmail.com>
2026-05-13 22:26:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ import streamlit.components.v1 as components
 # Sayfa ayarları (Sadece BİR KERE yazılmalı)
 st.set_page_config(page_title="UPS Delivery Panel", page_icon="logo.png", layout="centered")
 # --- ALTAR YENİ: MOBİL UYGULAMA (PWA) DESTEĞİ ---
+# --- MOBİL UYGULAMA (PWA) İKON DESTEĞİ ---
+st.markdown(
+    """
+    <link rel="manifest" href="https://raw.githubusercontent.com/ozoglualtar2-cloud/ups-kurye-app/main/manifest.json">
+    <link rel="apple-touch-icon" href="https://raw.githubusercontent.com/ozoglualtar2-cloud/ups-kurye-app/main/logo.png">
+    """,
+    unsafe_allow_html=True
+)
 # Linkleri direkt senin GitHub deponun içindeki dosyalara yönlendirdim
 st.markdown(
     """
